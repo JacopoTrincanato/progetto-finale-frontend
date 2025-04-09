2486a73899028b27c63a12c2dee2a3e3d4f1c37a
JacopoTrincanato <trincanatoj@gmail.com>
2025-04-09 20:48:25 +0200

    add style to header

--- react_cars/src/components/AppHeader.jsx
+++ react_cars/src/components/AppHeader.jsx
@@ -3,11 +3,12 @@ export default function AppHeader() {
 
     return (
         <header>
-            <nav className="navbar navbar-expand-lg">
-                <div className="container-fluid">
-                    <NavLink to='/' className="navbar-brand"><img src="https://static.vecteezy.com/system/resources/thumbnails/032/752/217/small/super-car-logo-icon-ai-generative-free-png.png" className='w-50' /></NavLink>
+            <nav className="navbar navbar-expand-lg container d-flex justify-content-between align-items-center">
 
-                </div>
+                <h1 className='text-white'>TJ cars</h1>
+
+                <NavLink to='/' className="navbar-brand"><img src="https://static.vecteezy.com/system/resources/thumbnails/032/752/217/small/super-car-logo-icon-ai-generative-free-png.png" className='w-50' /></NavLink>
+
             </nav>
         </header>
     )
