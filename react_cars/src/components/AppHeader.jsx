07b77c799adc0597d649fab3ab568704ebd0e127
JacopoTrincanato <trincanatoj@gmail.com>
2025-04-09 20:44:44 +0200

    create accordions for btn of optionals

--- react_cars/src/components/AppHeader.jsx
+++ react_cars/src/components/AppHeader.jsx
@@ -6,13 +6,7 @@ export default function AppHeader() {
             <nav className="navbar navbar-expand-lg">
                 <div className="container-fluid">
                     <NavLink to='/' className="navbar-brand"><img src="https://static.vecteezy.com/system/resources/thumbnails/032/752/217/small/super-car-logo-icon-ai-generative-free-png.png" className='w-50' /></NavLink>
-                    <div className="collapse navbar-collapse">
-                        <ul className="navbar-nav ms-auto">
-                            <li className="nav-item">
-                                <NavLink to='/' className="nav-link">Cars</NavLink>
-                            </li>
-                        </ul>
-                    </div>
+
                 </div>
             </nav>
         </header>
--- react_cars/src/pages/CarDetails.jsx
+++ react_cars/src/pages/CarDetails.jsx
@@ -8,6 +8,9 @@ export default function CarDetails() {
     //creo una costante per la car
     const [car, setCar] = useState({});
 
+    //creo una costante per lo stato dei bottoni
+    const [active, setActive] = useState(null)
+
     //ottengo il parametro id dal path
     const { id } = useParams();
 
@@ -30,48 +33,63 @@ export default function CarDetails() {
     //faccio la chiamata solo al caricamento del componente
     useEffect(fetchData, []);
 
-    console.log(car);
+    //funzione che reagisce al click del bottone
+    function handleClick(e) {
 
+        const newActive = e.target.getAttribute('data-index')
+
+        setActive(newActive)
+
+        //per richiuderle al click
+        if (active == newActive) {
+            setActive(null)
+        }
+    }
 
     return (
-        <>
+        <div className="container bg-white p-5">
 
-            <div className="container text-center mt-3">
+            <div className="container text-center">
                 <h1>Detalis of {car.model}</h1>
             </div>
 
-            <section className="container m-auto mt-3 p-4 d-flex gap-3 bg-white">
+            <section className="m-auto mt-5 d-flex gap-3">
 
                 <img src={car.urlImage} className="w-50" />
                 <div className="car-informations-top">
                     <h3 className="mb-3">Description: {car.description}</h3>
-                    <div>
-
-
-                        {car.optionals?.length > 0 ? (
-                            <div>
-                                <h3>Optionals:</h3>
-                                <ul>
-                                    {car.optionals.map((optional) => (
-                                        <li key={optional.id}>
-                                            <strong>{optional.name}</strong>: {optional.description}
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                        ) : (
-                            <div className="alert alert-warning">
-                                <h3>This car hasn't got optionals</h3>
-                            </div>
-                        )}
-
-                    </div>
 
                 </div>
 
             </section>
 
-            <section className="car-informations container m-auto p-4 gap-3 bg-white">
+            <section className="optionals mx-auto mt-5">
+
+                <h2 className="text-center">Optionals:</h2>
+
+                {car.optionals?.length > 0 ? (
+                    <div className="row d-flex justify-content-around rounded p-2">
+
+                        {car.optionals.map((optional, index) => (
+                            <div className="accordion-item col-md-3">
+                                <button key={optional.id} onClick={handleClick} data-index={index} className="btn btn-light">
+                                    {optional.name}
+                                </button>
+
+                                <h6 className={active == index ? 'active' : 'hide'}>{optional.description}</h6>
+                            </div>
+                        ))}
+
+                    </div>
+                ) : (
+                    <div className="alert alert-warning">
+                        <h3>This car hasn't got optionals</h3>
+                    </div>
+                )}
+
+            </section>
+
+            <section className="car-informations-bottom mx-auto mt-5">
                 <div className="row d-flex justify-content-around border border-lighter rounded p-2">
                     <div className="col-md-4">
                         <div className="text-center">
@@ -108,6 +126,6 @@ export default function CarDetails() {
             </section>
 
 
-        </>
+        </div>
     )
 }
